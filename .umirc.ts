c7c3ed5321c1eb4498bead5e642ddd361bb1cf36
tcdw <tcdw2011@gmail.com>
2023-08-20 14:54:58 +0800

    removed css in js due to performance problem

--- .umirc.ts
+++ .umirc.ts
@@ -3,13 +3,6 @@ import path from "node:path";
 
 export default defineConfig({
     npmClient: 'pnpm',
-    // https://github.com/umijs/umi/issues/11501
-    extraBabelPresets: [
-        path.join(
-            path.dirname(require.resolve('@emotion/babel-preset-css-prop/package.json')),
-            'dist/emotion-babel-preset-css-prop.cjs.js'
-        )
-    ],
     chainWebpack(config) {
         config.module.rule("mjscfg")
             .test(/\.m?js/)
